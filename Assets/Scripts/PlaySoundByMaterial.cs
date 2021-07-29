272d66ec5f473cc76925903003dc8d7bf2fccbbc
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-07-29 19:52:19 +0500

    Test play by Physic Material

---
+++ Assets/Scripts/PlaySoundByMaterial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMonoNode;
+
+[ExecuteInEditMode]
+public class PlaySoundByMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private XSoundNodeGraph graph = null;
+    [SerializeField]
+    private PhysicMaterial material = null;
+
+    private void OnEnable()
+    {
+        if (graph != null)
+        {
+            graph.Execute("Start", material);
+        }
+    }
+}
