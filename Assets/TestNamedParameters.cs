ed8d9f9d8d40214d6125a0fec3e60814302476d7
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-08-09 16:36:30 +0500

    Named flow parameters

---
+++ Assets/TestNamedParameters.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMonoNode;
+
+public class TestNamedParameters : MonoBehaviour
+{
+
+    [SerializeField]
+    private XSoundNodeGraph sound = null;
+
+    [SerializeField, Range(0f, 1f)]
+    private float           volume = 0.8f;
+
+    [SerializeField, Range(0.1f, 2f)]
+    private float           pitch = 0.8f;
+
+
+    private void Start()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            sound.Flow();
+        }
+    }
+
+    private void OnDisable()
+    {
+        sound.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sound.UpdateParameters(new Dictionary<string, object>
+        {
+            { nameof(volume), volume },
+            { nameof(pitch),  pitch }
+        });
+    }
+}
