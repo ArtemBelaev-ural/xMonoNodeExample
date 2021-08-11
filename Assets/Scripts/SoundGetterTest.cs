6a16a6fbe3c8d76a6829867c97bff1cb761b34b2
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-08-11 12:54:33 +0500

    update package

--- Assets/Scripts/SoundGetterTest.cs
+++ Assets/Scripts/SoundGetterTest.cs
@@ -10,15 +10,34 @@ public class SoundGetterTest : MonoBehaviour
     [SerializeField]
     private XSoundGetter sound = new XSoundGetter("Sounds/XContainers", "sounds.containers.main", "");
 
+    [SerializeField]
+    private Transform mesh;
+    [SerializeField]
+    private WheelCollider wheel;
+
+    [SerializeField]
+    private float motorTorque = 0.0f;
+    
+    [Header("Debug")]
+    [SerializeField]
+    private float rpm;
+    [SerializeField]
+    private float brakeTorcue;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        wheel.motorTorque = motorTorque;
+        rpm = wheel.rpm;
+        brakeTorcue = wheel.brakeTorque;
+
+
+        wheel.GetWorldPose(out Vector3 pos, out Quaternion rot);
+        mesh.localRotation = rot; //21;
     }
 }
